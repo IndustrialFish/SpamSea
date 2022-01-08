6146b91f2c29ca20c442f34d337adb8f2725f7d6
IndustrialFish <97189368+IndustrialFish@users.noreply.github.com>
2022-01-08 16:02:42 +0000

    Updated the URL Scanner Module

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import time
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-import csv
+from csv import writer
+from datetime import datetime
 
 
 def check_website_status(url):
@@ -17,18 +18,37 @@ def check_website_status(url):
 
 
 def check_number_variants(collectionName):
+
+    with open('Fraudulent Collections.csv', 'a', newline='') as write_obj:
+        csv_writer = writer(write_obj)
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        row = ['URL', 'Time']
+        csv_writer.writerow(row)
+
     for x in range(10000):
 
         modified_url = 'https://opensea.io/collection/' + collectionName + '-' + str(x)
-
         status_code = check_website_status(modified_url)
 
         if status_code == 200:
 
-            with open('Fraudulent Collections.csv', 'a+') as f:
-                f.write(modified_url)
+            with open('Fraudulent Collections.csv', 'a', newline='') as write_obj:
+                csv_writer = writer(write_obj)
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                row = [modified_url, current_time]
+                csv_writer.writerow(row)
 
             print('200 :' + modified_url)
 
+    print('---------------------')
+    print('All Collections Found')
+    print('---------------------')
+
+
+print('ScamSea Running...')
+print('------------------')
+print('Checking for collections...')
 
 check_number_variants("cryptopunk")
