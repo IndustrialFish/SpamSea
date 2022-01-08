8863acfc928c4483db97461462bc9bb8602faf7b
IndustrialFish <97189368+IndustrialFish@users.noreply.github.com>
2022-01-08 18:19:49 +0000

    Added Comments

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from csv import writer
 from datetime import datetime
 
 
-def check_website_status(url):
+def check_website_status(url):  # This function will return the status code of a website.
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15',
         'Accept-Language': 'en-gb'}
@@ -17,7 +17,8 @@ def check_website_status(url):
     return status_code
 
 
-def check_number_variants(collectionName):
+def check_number_variants(collectionName):  # Iterate using the following convention https://opensea.io/collection/{
+    # collectionName}-{x}
 
     with open('Suspect Collections.csv', 'a', newline='') as write_obj:
         csv_writer = writer(write_obj)
@@ -32,7 +33,6 @@ def check_number_variants(collectionName):
         status_code = check_website_status(modified_url)
 
         if status_code == 200:
-
             with open('Suspect Collections.csv', 'a', newline='') as write_obj:
                 csv_writer = writer(write_obj)
                 now = datetime.now()
@@ -54,7 +54,7 @@ print('Checking for collections...')
 check_number_variants("cryptopunk")
 
 # PLAN
-#-----------------------------#
+# -----------------------------#
 # Find images on page
 # Download images
 # Download all Collection images
