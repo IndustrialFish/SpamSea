b0c969dd8b50c80e777545392aa06e83c0f5f097
AnonFish <75339327+AnonFish@users.noreply.github.com>
2022-01-08 04:07:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import urllib
+import time
+
+import requests
+
 
 def check_website_status(url):
-    status_code = urllib.request.urlopen(url).getcode()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15',
+        'Accept-Language': 'en-gb',
+        'referer': 'https://google.com'}
+
+    request_response = requests.get(url, headers)
+    status_code = request_response.status_code
     website_is_up = status_code == 200
+    return status_code
 
 
+while True:
+    print(check_website_status("https://opensea.io/collection/cryptopunk-50"))
+    time.sleep(1)
