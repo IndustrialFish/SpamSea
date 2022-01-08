6ea679fd5a370a739996a9b37abb482bbc7b34ac
IndustrialFish <97189368+IndustrialFish@users.noreply.github.com>
2022-01-08 15:33:50 +0000

    Updated Code

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import urllib
 import time
-
+import pandas as pd
+from bs4 import BeautifulSoup
 import requests
+import csv
 
 
 def check_website_status(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15',
-        'Accept-Language': 'en-gb',
-        'referer': 'https://google.com'}
+        'Accept-Language': 'en-gb'}
 
-    request_response = requests.get(url, headers)
+    request_response = requests.get(url, headers=headers)
     status_code = request_response.status_code
-    website_is_up = status_code == 200
     return status_code
 
 
-while True:
-    print(check_website_status("https://opensea.io/collection/cryptopunk-50"))
-    time.sleep(1)
+def check_number_variants(collectionName):
+    for x in range(10000):
+
+        modified_url = 'https://opensea.io/collection/' + collectionName + '-' + str(x)
+
+        status_code = check_website_status(modified_url)
+
+        if status_code == 200:
+
+            with open('Fraudulent Collections.csv', 'a+') as f:
+                f.write(modified_url)
+
+            print('200 :' + modified_url)
+
+
+check_number_variants("cryptopunk")
